8f463e95c2119f2b4266a4a597d8da476eb67d82
tunghn0918 <tunghn0918@gmail.com>
2026-04-23 14:18:40 +0700

    airtable.js

--- api/airtable.js
+++ api/airtable.js
@@ -3,7 +3,7 @@ const TABLE  = process.env.AT_TABLE || "tblfMkw5lLEadlBO1";
 const TOKEN  = process.env.AT_TOKEN || "patMT1rjQzKR46R4A";
 const AT_URL = `https://api.airtable.com/v0/${BASE}/${TABLE}`;
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
